d938b5868cc87f46f3bf3fec372fd5e0d671121f
Potite_Bulle <135218431+PotiteBulle@users.noreply.github.com>
2025-04-07 21:43:35 +0200

    src/types.ts
    Ajout types Infractions

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,7 @@ export type Signalement = {
     url: string;
     // Type d’infraction signalée
     // Les valeurs sont limitées à 4 types :
-    type: 'pédocriminalité' | 'grooming' | 'harcèlement' | 'autre';
+    type: 'exemple d\'infractions : https://www.esiee-it.fr/fr/cybermalveillance-top-10-des-infractions-numeriques'; // (EXEMPLE D'INFRACTIONS)
     // Description libre : contexte, informations complémentaires, captures (texte)
     description: string;
   };  
